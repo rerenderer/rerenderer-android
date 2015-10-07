ce82b18157e64e57f176f5b7d6f4dd50720edcb9
nvbn <nvbn.rm@gmail.com>
2015-10-07 22:18:00 +0800

    Add support of `[:free var]` command

--- app/src/main/kotlin/com/nvbn/tryrerenderer/Interpreter.kt
+++ app/src/main/kotlin/com/nvbn/tryrerenderer/Interpreter.kt
@@ -39,12 +39,15 @@ data class Call(val resultVar: String, val objVar: String, val method: String,
 
 data class Get(val resultVar: String, val objVar: String, val attr: String) : Command()
 
+data class Free(val objVar: String) : Command()
+
 
 class Stopped : Exception()
 
 class Interpreter {
     val TAG = "INTERPRETE"
     var callId = 0
+    var vars = mapOf<String, Any?>()
 
     fun call(script: Collection<List<Any>>, rootId: String): Bitmap {
         callId += 1
@@ -66,14 +69,16 @@ class Interpreter {
                         line.get(1) as String,
                         line.get(2) as String,
                         line.get(3) as String)
+                ":free" -> Free(line.get(1) as String)
                 else -> throw Exception("Unknown action $line")
             }
         }
-        return actions.fold(mapOf<String, Any?>(), { vars, action ->
+        vars = actions.fold(vars, { vars, action ->
             if (callId != currentCallId)
                 throw Stopped()
             interpeteLine(vars, action)
-        }).get(rootId) as Bitmap
+        })
+        return vars.get(rootId) as Bitmap
     }
 
     fun prepareArg(vars: Map<String, Any?>, arg: VarOrVal): Any? = when (arg) {
@@ -87,15 +92,15 @@ class Interpreter {
     }
 
     fun interpeteLine(vars: Map<String, Any?>, line: Command): Map<String, Any?> {
-        Log.d(TAG, line.toString())
         return when (line) {
             is New -> vars.plus(line.resultVar to doNew(
                     vars, line.cls, line.args))
             is Call -> vars.plus(line.resultVar to doCall(
-                    vars, vars.getOrElse(line.objVar, {-> line.objVar}),
+                    vars, vars.getOrElse(line.objVar, { -> line.objVar }),
                     line.method, prepareArgs(vars, line.args)))
             is Get -> vars.plus(line.resultVar to doGet(
                     vars, line.objVar, line.attr))
+            is Free -> vars.minus(line.objVar)
             else -> vars
         }
     }
