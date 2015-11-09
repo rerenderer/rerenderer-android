511f4c1c996c287ab06b150f83b009646bace74a
nvbn <nvbn.rm@gmail.com>
2015-11-09 19:00:02 +0800

    Fix loading local assets

--- app/src/main/kotlin/com/nvbn/tryrerenderer/RerendererLoader.kt
+++ app/src/main/kotlin/com/nvbn/tryrerenderer/RerendererLoader.kt
@@ -1,14 +1,26 @@
 package com.nvbn.tryrerenderer
 
+import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.util.Log
 import java.net.URL
 
 object RerendererLoader {
+    val TAG = "RERENDERER_LOADER"
+    var context: Context? = null
+
     fun bitmapFromUrl(url: String): Bitmap {
-        val connection = URL(url).openConnection()
-        connection.connect()
-        val stream = connection.getInputStream()
-        return BitmapFactory.decodeStream(stream)
+        Log.d(TAG, "Load asset $url")
+
+        if (url.startsWith("file:///")) {
+            val name = url.replace("file:///android_asset/", "")
+            val asset = context!!.assets.open(name)
+            return BitmapFactory.decodeStream(asset)
+        } else {
+            val connection = URL(url).openConnection()
+            connection.connect()
+            return BitmapFactory.decodeStream(connection.inputStream)
+        }
     }
 }
